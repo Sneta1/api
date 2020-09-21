8e8df3592cd7fffb2941bef22a43ce1fbb9e84ea
sneta kayastha <esneta@elan.elantecs.com>
2020-09-21 13:14:19 -0400

    Initial commit

---
+++ api/reminders.php
@@ -0,0 +1,49 @@
+<?php
+class Reminder{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "api";
+ 
+    // object properties
+    public $id;
+    public $timee;
+    public $message;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    // signup user
+    function signup(){
+
+        $query = "INSERT INTO ". $this->table_name. " (time, message) VALUES('$this->timee', '$this->message')";
+
+        $stmt = $this->conn->prepare($query);
+
+        if($stmt->execute()){
+        $this->id = $this->conn->lastInsertId();
+        return true;
+        }
+        return false;
+    }
+
+    function show(){
+
+       $query = "SELECT *FROM api where id=(SELECT LAST_INSERT_ID()"; 
+       $stmt = $this->conn->prepare($query);
+    
+        if($stmt->execute()){
+           $result= $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+           while($row= $stmt->fetch()){
+            echo $row['time'] . "<br/>" ;
+            echo $row['message'];
+        }
+        return true;
+    }
+
+      return false;
+    }
+  }
+?>
---
+++ api/webservice.php
@@ -0,0 +1,45 @@
+<?php
+ 
+// get database connection
+include_once 'config.php';
+ 
+// instantiate user object
+include_once 'reminders.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$reminder = new Reminder($db);
+ 
+// set user property values
+
+
+ 
+// create the user
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+    
+if($reminder->signup()){
+    $reminder->timee = $_POST['timee'];
+    $reminder->message = $_POST['message'];
+    $reminder_arr=array(
+        "status" => true,
+        "message" => $reminder->message,
+        "time" => $reminder->timee,
+    );
+}
+}
+/*else if($reminder->show()){
+    $reminder->timee = $_GET['timee'];
+    $reminder->message = $_GET['message'];
+     $reminder_arr=array(
+        "status" => true,
+        "message" => $reminder->message,
+        "time" => $reminder->timee,
+    );
+    
+}*/
+print_r(json_encode($reminder_arr));
+    //$user->username = $_GET['username'];
+    //$user->password = $_GET['password'];
+
+?>
